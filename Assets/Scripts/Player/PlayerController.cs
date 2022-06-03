b530579e018bf8974215322808761e331b140924
Adrián de la Torre Rodríguez <atr_96@hotmail.com>
2022-06-03 11:57:06 +0200

    Added player controller

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float WalkingSpeed = 7.5f;
+    public float RunningSpeed = 11.5f;
+    public float Gravity = 20.0f;
+
+    [Header("Look")]
+    public Camera PlayerCamera;
+    public float LookSpeed = 2.0f;
+    public float LookXLimit = 45.0f;
+
+    private CharacterController _characterController;
+    private Vector3 _moveDirection = Vector3.zero;
+    private float _rotationX = 0;
+
+    [HideInInspector]
+    public bool canMove = true;
+
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        CalculateMovement();
+        ApplyGravity();
+        Move();
+        Look();
+    }
+
+    private void CalculateMovement()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+
+        float curSpeedX = canMove ? (isRunning ? RunningSpeed : WalkingSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? RunningSpeed : WalkingSpeed) * Input.GetAxis("Horizontal") : 0;
+        _moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+    }
+
+    private void ApplyGravity()
+    {
+        _moveDirection.y -= Gravity * Time.deltaTime;
+    }
+
+    private void Move()
+    {
+        _characterController.Move(_moveDirection * Time.deltaTime);
+    }
+
+    private void Look()
+    {
+        if (canMove)
+        {
+            _rotationX += -Input.GetAxis("Mouse Y") * LookSpeed;
+            _rotationX = Mathf.Clamp(_rotationX, -LookXLimit, LookXLimit);
+            PlayerCamera.transform.localRotation = Quaternion.Euler(_rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * LookSpeed, 0);
+        }
+    }
+}
--- Assets/Scripts/VFX/PlayerPositionInMaterial.cs
+++ Assets/Scripts/VFX/PlayerPositionInMaterial.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class PlayerPositionInMaterial : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Material[] Materials;
+    public string PlayerPositionParameterName = "_PlayerPosition";
+    public bool ApplyPositionOnStart;
+
+    private bool _applyPosition;
+
+    private void Start()
     {
-        
+        ApplyPosition(ApplyPositionOnStart);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (_applyPosition)
+        {
+            SetParameter(transform.position);
+        }
+    }
+
+    private void SetParameter(Vector3 position)
+    {
+        foreach (var material in Materials)
+        {
+            material.SetVector(PlayerPositionParameterName, position);
+        }
+    }
+
+    public void ApplyPosition(bool apply)
+    {
+        _applyPosition = apply;
+
+        if (!apply)
+        {
+            SetParameter(Vector3.one * Mathf.Infinity);
+        }
     }
 }
