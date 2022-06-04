36bc242a3c4fcf3b4fda8f5921a10bb7d4e5e167
Adrián de la Torre Rodríguez <atr_96@hotmail.com>
2022-06-04 21:44:32 +0200

    Added TV event

--- Assets/Scripts/Events/TV.cs
+++ Assets/Scripts/Events/TV.cs
@@ -5,18 +5,35 @@ using UnityEngine;
 public class TV : ChecklistEvent
 {
     public float WatchTime;
+    public Renderer TVScreen;
+    public Transform LookAt;
+
 
     public override void CompleteEvent()
     {
         base.CompleteEvent();
         _player.CanMove = false;
-        _player.LookAt(transform.position);
+        _player.LookAt(LookAt.position);
+        TurnOnTv(true);
         Invoke(nameof(EndEvent), WatchTime);
     }
 
     private void EndEvent()
     {
         _player.CanMove = true;
+        TurnOnTv(false);
         LevelChecklistManager.Instance.CompleteEvent(EventName);
+    }
+
+    private void TurnOnTv(bool turnOn)
+    {
+        int value = turnOn ? 1 : 0;
+        foreach (var material in TVScreen.materials)
+        {
+            if (material.HasInt("_On"))
+            {
+                material.SetInt("_On", value);
+            }
+        }
     }
 }
