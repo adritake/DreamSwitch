5496af84a748a31962c7cac0897807aa2e4d5464
Adrián de la Torre Rodríguez <atr_96@hotmail.com>
2022-06-04 14:11:10 +0200

    Breakfast Event

---
+++ Assets/Scripts/Events/Breakfast.cs
@@ -0,0 +1,27 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakfast : ChecklistEvent
+{
+    public float EatingTime;
+    public GameObject Toast;
+    public Transform ToastEndPosition;
+    public float ClothesMovingTime;
+
+
+    public override void CompleteEvent()
+    {
+        base.CompleteEvent();
+        _player.CanMove = false;
+        Toast.transform.DOMove(ToastEndPosition.position, ClothesMovingTime).SetEase(Ease.InOutQuad);
+        Invoke(nameof(EndEvent), EatingTime);
+    }
+
+    private void EndEvent()
+    {
+        _player.CanMove = true;
+        Destroy(Toast);
+    }
+}
