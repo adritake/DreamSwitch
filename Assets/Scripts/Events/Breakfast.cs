9220a4a31c1e43ea9c5d6a8fa93ad97dfddde110
Leamsy <ismamoyano97@gmail.com>
2022-06-13 12:33:17 +0200

    Tostadeo

--- Assets/Scripts/Events/Breakfast.cs
+++ Assets/Scripts/Events/Breakfast.cs
@@ -7,8 +7,18 @@ public class Breakfast : ChecklistEvent
 {
     public float EatingTime;
     public GameObject Toast;
+    public Transform ToastMidPosition;
     public Transform ToastEndPosition;
     public float ClothesMovingTime;
+
+    Sequence seq;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        seq = DOTween.Sequence();
+    }
 
 
     public override bool CompleteEvent()
@@ -18,8 +28,11 @@ public class Breakfast : ChecklistEvent
             return false;
         }
         _player.CanMove = false;
-        Toast.transform.DOMove(ToastEndPosition.position, ClothesMovingTime).SetEase(Ease.InOutQuad);
-        FMODUnity.RuntimeManager.PlayOneShotAttached("event:/Sfx/Loop1/Toast", gameObject);
+
+        seq.Append(Toast.transform.DOMove(ToastMidPosition.position, ClothesMovingTime/2).SetEase(Ease.InOutQuad))
+            .Append(Toast.transform.DOMove(ToastEndPosition.position, ClothesMovingTime/2).SetEase(Ease.InOutQuad));
+        //Toast.transform.DOMove(ToastEndPosition.position, ClothesMovingTime).SetEase(Ease.InOutQuad);
+        //FMODUnity.RuntimeManager.PlayOneShotAttached("event:/Sfx/Loop1/Toast", gameObject);
         Invoke(nameof(EndEvent), EatingTime);
         return true;
     }
