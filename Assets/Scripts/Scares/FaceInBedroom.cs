c4b681130fe346c86df9f28370ece9c99ac56d5e
Adrián de la Torre Rodríguez <atr_96@hotmail.com>
2022-07-04 12:12:00 +0200

    bicho en el sueño 2, ahora desaparece mas rapido

--- Assets/Scripts/Scares/FaceInBedroom.cs
+++ Assets/Scripts/Scares/FaceInBedroom.cs
@@ -5,41 +5,20 @@ using DG.Tweening;
 
 public class FaceInBedroom : MonoBehaviour
 {
-    public GameObject face;
+    public Renderer Shadow;
+    public Renderer Deform;
+    public float VanishTime;
+    public float VanishDuration;
 
-    public Renderer shadow;
-    public Renderer deform;
-
-    Sequence seq;
-
+   
     void Start()
     {
-        seq = DOTween.Sequence();
-
-        seq.Append(face.transform.DOMoveY(0.2f, 1.5f).SetEase(Ease.OutQuad).SetRelative())
-            .AppendInterval(0.05f)
-            .Append(face.transform.DOMoveY(-0.2f, 1.5f).SetEase(Ease.OutQuad).SetRelative())
-            .AppendInterval(0.05f)
-            .SetLoops(-1)
-            .Play();
-            
-        Invoke("GoAway", 9);
-    }
-
-    void Update()
-    {
-
+        Invoke(nameof(GoAway), VanishTime);
     }
 
     void GoAway()
     {
-        seq.Kill();
-
-        shadow.material.DOFloat(0, "_Alpha", 0.5f);
-        deform.material.DOFloat(0, "_Alpha", 0.5f);
-        
-        /* face.transform.DOMoveX(-5, 2f).SetRelative().OnComplete(() => {
-            Destroy(gameObject);
-        }); */
+        Shadow.material.DOFloat(0, "_Alpha", VanishDuration);
+        Deform.material.DOFloat(0, "_Alpha", VanishDuration);
     }
 }
